7592da545fdebed892517881f4670824885f4faf
VictorLCT952 <57416797+VictorLCT952@users.noreply.github.com>
2020-01-18 15:18:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const settings = require("./your_settings.json")
 const bot = new Discord.Client()
 bot.on('ready', async => {
 console.log("Rainbow bot is ready!" + "\n" + bot.user.tag + "\n" + "Server Count: "  + bot.guilds.size + "\n" + "Cached users: " + bot.users.size + "\n" + "Enjoy!")
-bot.user.setActivity('Rainbow In 69 servers', { type: 'PLAYING'}).catch(console.error);
+bot.user.setActivity('Rainbow In 69 Servers', { type: 'PLAYING'}).catch(console.error);
 bot.user.setStatus('dnd')
 });
 
